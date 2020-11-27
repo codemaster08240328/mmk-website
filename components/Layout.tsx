5b0d3ab308c24650ecc27275998e387c88b4f0ae
Code Master <codemaster@Codes-MacBook-Pro.local>
2020-11-27 15:43:53 +0300

    add fonts

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import Footer from './Footer';
 import './layout.scss';
 
@@ -10,7 +10,7 @@ type Props = {
 };
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => {
-  const router = useRouter();
+  // const router = useRouter();
   return (
     <div className="mmk-website-layout">
       <Head>
@@ -18,6 +18,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => {
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <link rel="shortcut icon" href="/static/favicon.ico"></link>
+        <link rel="stylesheet" href="/fonts/font-style.css"/>
       </Head>
       {children}
       <Footer />
